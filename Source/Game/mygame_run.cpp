298ead015c719dda5e8b7e9aa1ea1345af6ff4ed
Chinococo <garyliuyahoocom384@gmail.com>
2024-03-15 05:33:56 +0800

    測試

--- Source/Game/mygame_run.cpp
+++ Source/Game/mygame_run.cpp
@@ -38,6 +38,8 @@ void CGameStateRun::OnMove()							// 移動遊戲元素
 
 void CGameStateRun::OnInit()  								// 遊戲的初值及圖形設定
 {
+	//Raiden::GameObjectPool<int> test;
+	//test.AddElement({ 10,10 });
 	components.push_back(std::make_unique<Raiden::Background>());
 	for (const std::unique_ptr<Raiden::Component> &component : components)
 		component->Init();

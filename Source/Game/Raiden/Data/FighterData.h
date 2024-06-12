1d4816150bc81b26b14d8c67a9e5f737a81cfdf0
Chinococo <garyliuyahoocom384@gmail.com>
2024-06-13 06:05:53 +0800

    新增可以讓設定戰機發射角度跟速度

--- Source/Game/Raiden/Data/FighterData.h
+++ Source/Game/Raiden/Data/FighterData.h
@@ -9,6 +9,9 @@ namespace Raiden
 	{
 		int appear_distance;
 		int move_interval_milli;
+		int bullet_speed;
+		int bullet_type;
+		int bullet_angle;
 		std::tuple<int, int, int> color_mask;
 		std::vector<std::string> sprites;
 		std::vector<CPoint> positions;
--- Source/Game/Raiden/RunningState/RunningState.cpp
+++ Source/Game/Raiden/RunningState/RunningState.cpp
@@ -56,6 +56,7 @@ namespace Raiden {
 							continue;
 						}
 					}
+
 				}
 
 				
